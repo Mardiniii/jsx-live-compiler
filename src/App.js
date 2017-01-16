945cfdfd8d21cd794da58fceab718d84abd44729
mardiniii <sezama03@hotmail.com>
2017-01-15 19:04:54 -0500

    Define App component constructor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: '/* Write your JSX code here*/',
+      output: '',
+      err: ''
+    }
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea
+          onChange={this.update.bind(this)}
+          defaultValue={this.state.input}/>
+          <pre>
+            {this.state.output}
+          </pre>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
